2697e3e7319a4138c7f54e141595600e6341c4d8
wiktbob992 <wiktbob992@student.polsl.pl>
2023-10-04 19:45:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gabinet_psychologiczny/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.gabinet_psychologiczny;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.gabinet_psychologiczny.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setSelectedItemId(R.id.calendar); //default fragment
+        replaceFragment(new CalendarFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+
+                case R.id.patients:
+                    replaceFragment(new PatientFragment());
+                    break;
+                case R.id.calendar:
+                    replaceFragment(new CalendarFragment());
+                    break;
+                case R.id.bills:
+                    replaceFragment(new BillsFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
